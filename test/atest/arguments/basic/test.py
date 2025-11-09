5ca09c0878a4fb971c5527169cb3667b4a6f9569
Sander van Beek <lakitna@gmail.com>
2025-11-09 11:32:29 +0100

    Added atests for arguments command

---
+++ test/atest/arguments/basic/test.py
@@ -0,0 +1,24 @@
+from test.atest.utils import AcceptanceTest
+
+
+class TestCommandAcceptance(AcceptanceTest):
+    def test_arguments_command(self):
+        self.run_test(
+            ["arguments", "./robot"],
+            "./expected_output.log",
+            __file__,
+        )
+
+    def test_arguments_command_with_count(self):
+        self.run_test(
+            ["arguments", "./robot", "--show-count"],
+            "./expected_output_count.log",
+            __file__,
+        )
+
+    def test_arguments_command_with_verbose(self):
+        self.run_test(
+            ["arguments", "./robot", "--verbose"],
+            "./expected_output_verbose.log",
+            __file__,
+        )
