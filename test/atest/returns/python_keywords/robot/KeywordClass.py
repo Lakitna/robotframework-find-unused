80639c308c98181d2408c16e07c89a63c20e5988
Sander van Beek <lakitna@gmail.com>
2025-11-19 17:14:26 +0100

    Added atest to pass

---
+++ test/atest/returns/python_keywords/robot/KeywordClass.py
@@ -0,0 +1,9 @@
+class KeywordClass:
+    def does_not_return_from_class() -> None:
+        """Do nothing"""
+
+    def returning_keyword_from_class():
+        """Return true"""
+        if True:
+            return True
+        return
