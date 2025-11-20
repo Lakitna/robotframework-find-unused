5b0ec912a6fd6c9682ef4382427b039a7cff505e
Sander van Beek <lakitna@gmail.com>
2025-11-20 14:39:14 +0100

    Made test work in robot 6

--- test/atest/variables/variable_files/robot/DynamicClassName.py
+++ test/atest/variables/variable_files/robot/DynamicClassName.py
@@ -7,7 +7,7 @@ class DynamicClassName:
         if True:
             variables["dynamic_var_from_python_dynamic_class_file"] = True
 
-        if user_arg is True:
+        if user_arg is not False:
             variables["dynamic_var_from_python_dynamic_class_file_using_user_args"] = True
 
         return variables
