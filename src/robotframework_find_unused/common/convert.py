ecc9a4d8b79f616e8c362575cd0838a7c3f38865
Lakitna <lakitna@gmail.com>
2025-12-23 20:54:38 +0100

    Changed how rel paths are made to support py 3.10

--- src/robotframework_find_unused/common/convert.py
+++ src/robotframework_find_unused/common/convert.py
@@ -1,4 +1,5 @@
 import functools
+import os
 from pathlib import Path
 from typing import Any, Literal, cast
 
@@ -40,7 +41,8 @@ def to_relative_path(parent: Path, child: Path) -> str:
     """
     Get relative file path from parent to child. Output suitable for user output.
     """
-    rel_path = child.resolve().relative_to(parent.resolve(), walk_up=True).as_posix()
+    rel_path = os.path.relpath(child.resolve(), parent.resolve())
+    rel_path = Path(rel_path).as_posix()
 
     if not rel_path.startswith("."):
         rel_path = f"./{rel_path}"
