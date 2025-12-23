3fec5bb224a7f1a251511aad2725daec3b07a574
Sander van Beek <lakitna@gmail.com>
2025-12-23 20:16:06 +0100

    Added some tests

---
+++ test/atest/files/basic/test.py
@@ -0,0 +1,19 @@
+from test.atest.utils import AcceptanceTest
+
+
+class TestCommandAcceptance(AcceptanceTest):
+    def test_files_command(self):
+        self.run_test(
+            ["files", "./robot"],
+            "./expected_output.log",
+            __file__,
+            expected_exit_code=1,
+        )
+
+    def test_files_command_tree(self):
+        self.run_test(
+            ["files", "./robot", "--show-tree"],
+            "./expected_output_tree.log",
+            __file__,
+            expected_exit_code=1,
+        )
