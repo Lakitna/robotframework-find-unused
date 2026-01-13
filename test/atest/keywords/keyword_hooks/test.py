54dce3ef3aba79fe9032c8f8f739ee0c13329a5e
Lakitna <lakitna@gmail.com>
2026-01-13 21:02:18 +0100

    Added proper way for skipping tests based on robot version

--- test/atest/keywords/keyword_hooks/test.py
+++ test/atest/keywords/keyword_hooks/test.py
@@ -1,17 +1,13 @@
-import pytest
-from robot.version import get_version
-
 from test.atest.utils import AcceptanceTest
 
 
 class TestCommandAcceptance(AcceptanceTest):
     def test_keywords_command_keyword_hooks(self):
-        if get_version()[0] == "6":
-            pytest.skip("Robot 6 does not support keyword setup keywords")
-
         self.run_test(
             ["keywords", "./robot", "--show-count"],
             "./expected_output.log",
             __file__,
             expected_exit_code=1,
+            # Robot 6 does not support keyword setup keywords
+            min_robot_version="7.0.0",
         )
