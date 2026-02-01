5b9cccd774c69019dc66af2aa6f7835306e64b1f
Lakitna <lakitna@gmail.com>
2026-02-01 11:54:16 +0100

    Fixed tests

--- src/robotframework_find_unused/common/sort.py
+++ src/robotframework_find_unused/common/sort.py
@@ -4,4 +4,4 @@ from .const import KeywordData
 def sort_keywords_by_name(unsorted: list[KeywordData]) -> list[KeywordData]:
     """Sort list of keyword by library name and keyword name"""
     # If no library, use space to put it at the front.
-    return sorted(unsorted, key=lambda kw: f"{kw.library or ' '}.{kw.normalized_name}")
+    return sorted(unsorted, key=lambda kw: f"{kw.library or 'aaa'}.{kw.normalized_name}")
